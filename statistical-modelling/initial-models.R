626ac2876cf54c31484779e368f15500300bc112
hendersontrent <trent.henderson1@outlook.com>
2020-05-05 20:18:55 +1000

    GAM vs GLM model comparisons

--- statistical-modelling/initial-models.R
+++ statistical-modelling/initial-models.R
@@ -32,19 +32,47 @@ if (!exists(keepers)) {
 # RESPONSE: Reassuring
 #---------------------
 
+# GAM
+
 model_reassure <- gam(mpfc_reassuring ~ s(t_fscrs_inad) + 
                         s(fears_of_expressing_compassion_to_self),
              data = d)
 summary(model_reassure)
 
+# GLM
+
+model_reassure_glm <- glm(mpfc_reassuring ~ t_fscrs_inad + 
+                        fears_of_expressing_compassion_to_self,
+                      data = d)
+summary(model_reassure_glm)
+
+# Model comparisons
+
+sum(abs(residuals(model_reassure)))
+sum(abs(residuals(model_reassure_glm)))
+
 #---------------------
 # RESPONSE: AI
 #---------------------
+
+# GAM
 
 model_ai <- gam(ai_reassurance ~ s(t_fscrs_inad) + 
                   s(fears_of_expressing_compassion_to_self),
                       data = d)
 summary(model_ai)
+
+# GLM
+
+model_ai_glm <- glm(ai_reassurance ~ t_fscrs_inad + 
+                            fears_of_expressing_compassion_to_self,
+                          data = d)
+summary(model_ai_glm)
+
+# Model comparisons
+
+sum(abs(residuals(model_ai)))
+sum(abs(residuals(model_ai_glm)))
 
 #-----------------DATA VIS & OUTPUTS--------------
 
