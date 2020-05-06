2f24203bc57d15e1ded9285b9089c9c28d11cc0b
hendersontrent <trent.henderson1@outlook.com>
2020-05-06 23:03:10 +1000

    Further reassurance modelling

--- statistical-modelling/key-relationship-model-selection.R
+++ statistical-modelling/key-relationship-model-selection.R
@@ -33,13 +33,15 @@ if (!exists(keepers)) {
 
 # GAM
 
-amyg_re_gam <- gam(amygdala_reassurance ~ s(s_dass_stress) + s(t_fscrs_inad) + s(t_neuroticism),
+amyg_re_gam <- gam(amygdala_reassurance ~ s(s_dass_stress) + s(t_fscrs_inad) + 
+                     s(fears_of_expressing_compassion_to_self),
                    data = d)
 summary(amyg_re_gam)
 
 # GLM
 
-amyg_re_glm <- glm(amygdala_reassurance ~ s_dass_stress + t_fscrs_inad + t_neuroticism,
+amyg_re_glm <- glm(amygdala_reassurance ~ s_dass_stress + t_fscrs_inad + 
+                     fears_of_expressing_compassion_to_self,
                    data = d)
 summary(amyg_re_glm)
 
@@ -49,7 +51,8 @@ sum(abs(residuals(amyg_re_glm)))
 # Bayes
 
 set.seed(123)
-amyg_re_bayes <- MCMCregress(amygdala_reassurance ~ 1 + s_dass_stress + t_fscrs_inad + t_neuroticism,
+amyg_re_bayes <- MCMCregress(amygdala_reassurance ~ 1 + s_dass_stress + t_fscrs_inad + 
+                               fears_of_expressing_compassion_to_self,
                                data = d, burnin = 1000, mcmc = 10000, thin = 1)
 
 par(mfrow = c(1,1))
@@ -63,13 +66,15 @@ summary(amyg_re_bayes)
 
 # GAM
 
-mpfc_re_gam <- gam(mpfc_reassuring ~ s(s_dass_stress) + s(t_fscrs_inad) + s(t_neuroticism),
+mpfc_re_gam <- gam(mpfc_reassuring ~ s(s_dass_stress) + s(t_fscrs_inad) + 
+                     s(fears_of_expressing_compassion_to_self),
                    data = d)
 summary(mpfc_re_gam)
 
 # GLM
 
-mpfc_re_glm <- glm(mpfc_reassuring ~ s_dass_stress + t_fscrs_inad + t_neuroticism,
+mpfc_re_glm <- glm(mpfc_reassuring ~ s_dass_stress + t_fscrs_inad + 
+                     fears_of_expressing_compassion_to_self,
                      data = d)
 summary(mpfc_re_glm)
 
@@ -79,7 +84,8 @@ sum(abs(residuals(mpfc_re_glm)))
 # Bayes
 
 set.seed(123)
-mpfc_re_bayes <- MCMCregress(mpfc_reassuring ~ 1 + s_dass_stress + t_fscrs_inad + t_neuroticism,
+mpfc_re_bayes <- MCMCregress(mpfc_reassuring ~ 1 + s_dass_stress + t_fscrs_inad + 
+                               fears_of_expressing_compassion_to_self,
                                data = d, burnin = 1000, mcmc = 10000, thin = 1)
 
 par(mfrow = c(1,1))
@@ -87,3 +93,68 @@ plot(mpfc_re_bayes) # Bayesian trace and density plots
 HPDinterval(mpfc_re_bayes) # Highest posterior density intervals
 summary(mpfc_re_bayes)
 
+#-------------------------------
+# RESPONSE: ACC reassurance
+#-------------------------------
+
+# GAM
+
+acc_re_gam <- gam(acc_reassurance ~ s(s_dass_stress) + s(t_fscrs_inad) + 
+                     s(fears_of_expressing_compassion_to_self),
+                   data = d)
+summary(acc_re_gam)
+
+# GLM
+
+acc_re_glm <- glm(acc_reassurance ~ s_dass_stress + t_fscrs_inad + 
+                     fears_of_expressing_compassion_to_self,
+                   data = d)
+summary(acc_re_glm)
+
+sum(abs(residuals(acc_re_gam)))
+sum(abs(residuals(acc_re_glm)))
+
+# Bayes
+
+set.seed(123)
+acc_re_bayes <- MCMCregress(acc_reassurance ~ 1 + s_dass_stress + t_fscrs_inad + 
+                               fears_of_expressing_compassion_to_self,
+                             data = d, burnin = 1000, mcmc = 10000, thin = 1)
+
+par(mfrow = c(1,1))
+plot(acc_re_bayes) # Bayesian trace and density plots
+HPDinterval(acc_re_bayes) # Highest posterior density intervals
+summary(acc_re_bayes)
+
+#-------------------------------
+# RESPONSE: AI reassurance
+#-------------------------------
+
+# GAM
+
+ai_re_gam <- gam(ai_reassurance ~ s(s_dass_stress) + s(t_fscrs_inad) + 
+                    s(fears_of_expressing_compassion_to_self),
+                  data = d)
+summary(acc_re_gam)
+
+# GLM
+
+ai_re_glm <- glm(ai_reassurance ~ s_dass_stress + t_fscrs_inad + 
+                    fears_of_expressing_compassion_to_self,
+                  data = d)
+summary(ai_re_glm)
+
+sum(abs(residuals(ai_re_gam)))
+sum(abs(residuals(ai_re_glm)))
+
+# Bayes
+
+set.seed(123)
+ai_re_bayes <- MCMCregress(ai_reassurance ~ 1 + s_dass_stress + t_fscrs_inad + 
+                              fears_of_expressing_compassion_to_self,
+                            data = d, burnin = 1000, mcmc = 10000, thin = 1)
+
+par(mfrow = c(1,1))
+plot(ai_re_bayes) # Bayesian trace and density plots
+HPDinterval(ai_re_bayes) # Highest posterior density intervals
+summary(ai_re_bayes)
