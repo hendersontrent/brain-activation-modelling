97bf0535aced74c88543d8a80dc54a58321daa33
hendersontrent <trent.henderson1@outlook.com>
2020-05-06 20:33:46 +1000

    Simplified models to just reassurance

--- statistical-modelling/key-relationship-model-selection.R
+++ statistical-modelling/key-relationship-model-selection.R
@@ -28,36 +28,6 @@ if (!exists(keepers)) {
 #-----------------STATISTICAL MODELLING---------
 
 #-------------------------------
-# RESPONSE: Amygdala criticism
-#-------------------------------
-
-# GAM
-
-amyg_crit_gam <- gam(amygdala_criticism ~ s(s_dass_stress) + s(t_fscrs_inad) + s(t_neuroticism),
-                data = d)
-summary(amyg_crit_gam)
-
-# GLM
-
-amyg_crit_glm <- glm(amygdala_criticism ~ s_dass_stress + t_fscrs_inad + t_neuroticism,
-                     data = d)
-summary(amyg_crit_glm)
-
-sum(abs(residuals(amyg_crit_gam)))
-sum(abs(residuals(amyg_crit_glm)))
-
-# Bayes
-
-set.seed(123)
-amyg_crit_bayes <- MCMCregress(amygdala_criticism ~ 1 + s_dass_stress + t_fscrs_inad + t_neuroticism,
-                     data = d, burnin = 1000, mcmc = 10000, thin = 1)
-
-par(mfrow = c(1,1))
-plot(amyg_crit_bayes) # Bayesian trace and density plots
-HPDinterval(amyg_crit_bayes) # Highest posterior density intervals
-summary(amyg_crit_bayes)
-
-#-------------------------------
 # RESPONSE: Amygdala reassurance
 #-------------------------------
 
@@ -88,36 +58,6 @@ HPDinterval(amyg_re_bayes) # Highest posterior density intervals
 summary(amyg_re_bayes)
 
 #-------------------------------
-# RESPONSE: MPFC criticism 
-#-------------------------------
-
-# GAM
-
-mpfc_crit_gam <- gam(mpfc_crit ~ s(s_dass_stress) + s(t_fscrs_inad) + s(t_neuroticism),
-                     data = d)
-summary(mpfc_crit_gam)
-
-# GLM
-
-mpfc_crit_glm <- glm(mpfc_crit ~ s_dass_stress + t_fscrs_inad + t_neuroticism,
-                   data = d)
-summary(mpfc_crit_glm)
-
-sum(abs(residuals(mpfc_crit_gam)))
-sum(abs(residuals(mpfc_crit_glm)))
-
-# Bayes
-
-set.seed(123)
-mpfc_crit_bayes <- MCMCregress(mpfc_crit ~ 1 + s_dass_stress + t_fscrs_inad + t_neuroticism,
-                             data = d, burnin = 1000, mcmc = 10000, thin = 1)
-
-par(mfrow = c(1,1))
-plot(mpfc_crit_bayes) # Bayesian trace and density plots
-HPDinterval(mpfc_crit_bayes) # Highest posterior density intervals
-summary(mpfc_crit_bayes)
-
-#-------------------------------
 # RESPONSE: MPFC reassurance
 #-------------------------------
 
@@ -146,34 +86,4 @@ par(mfrow = c(1,1))
 plot(mpfc_re_bayes) # Bayesian trace and density plots
 HPDinterval(mpfc_re_bayes) # Highest posterior density intervals
 summary(mpfc_re_bayes)
-
-#-------------------------------
-# RESPONSE: PCC criticism
-#-------------------------------
-
-# GAM
-
-pcc_crit_gam <- gam(crit_pcc ~ s(s_dass_stress) + s(t_fscrs_inad) + s(t_neuroticism),
-                    data = d)
-summary(pcc_crit_gam)
-
-# GLM
-
-pcc_crit_glm <- glm(crit_pcc ~ s_dass_stress + t_fscrs_inad + t_neuroticism,
-                     data = d)
-summary(pcc_crit_glm)
-
-sum(abs(residuals(pcc_crit_gam)))
-sum(abs(residuals(pcc_crit_glm)))
-
-# Bayes
-
-set.seed(123)
-pcc_crit_bayes <- MCMCregress(crit_pcc ~ 1 + s_dass_stress + t_fscrs_inad + t_neuroticism,
-                           data = d, burnin = 1000, mcmc = 10000, thin = 1)
-
-par(mfrow = c(1,1))
-plot(pcc_crit_bayes) # Bayesian trace and density plots
-HPDinterval(pcc_crit_bayes) # Highest posterior density intervals
-summary(pcc_crit_bayes)
 
